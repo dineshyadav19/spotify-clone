4001178033785e295ae33d0f99eb31747d8d94a2
dineshyadav19 <dinesh.2108.yadav@gmail.com>
2023-09-19 14:44:56 +0530

    feat: add useUserhook

---
+++ hooks/useUser.tsx
@@ -0,0 +1,89 @@
+import { useEffect, useState, createContext, useContext } from 'react';
+import {
+  useUser as useSupaUser,
+  useSessionContext,
+  User,
+} from '@supabase/auth-helpers-react';
+
+import { UserDetails, Subscription } from '@/types';
+
+type UserContextType = {
+  accessToken: string | null;
+  user: User | null;
+  userDetails: UserDetails | null;
+  isLoading: boolean;
+  subscription: Subscription | null;
+};
+
+export const UserContext = createContext<UserContextType | undefined>(
+  undefined
+);
+
+export interface Props {
+  [propName: string]: any;
+}
+
+export const UserContextProvider = (props: Props) => {
+  const {
+    session,
+    isLoading: isLoadingUser,
+    supabaseClient: supabase,
+  } = useSessionContext();
+  const user = useSupaUser();
+  const accessToken = session?.access_token ?? null;
+  const [isLoadingData, setIsloadingData] = useState(false);
+  const [userDetails, setUserDetails] = useState<UserDetails | null>(null);
+  const [subscription, setSubscription] = useState<Subscription | null>(null);
+
+  const getUserDetails = () => supabase.from('users').select('*').single();
+  const getSubscription = () =>
+    supabase
+      .from('subscriptions')
+      .select('*, prices(*, products(*))')
+      .in('status', ['trialing', 'active'])
+      .single();
+
+  useEffect(() => {
+    const checkConditionsForUserDetailsSubscription =
+      user && !isLoadingData && !userDetails && !subscription;
+
+    if (checkConditionsForUserDetailsSubscription) {
+      setIsloadingData(true);
+      Promise.allSettled([getUserDetails(), getSubscription()]).then(
+        (results) => {
+          const userDetailsPromise = results[0];
+          const subscriptionPromise = results[1];
+
+          if (userDetailsPromise.status === 'fulfilled')
+            setUserDetails(userDetailsPromise.value.data as UserDetails);
+
+          if (subscriptionPromise.status === 'fulfilled')
+            setSubscription(subscriptionPromise.value.data as Subscription);
+
+          setIsloadingData(false);
+        }
+      );
+    } else if (!user && !isLoadingUser && !isLoadingData) {
+      setUserDetails(null);
+      setSubscription(null);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user, isLoadingUser, isLoadingData]);
+
+  const value = {
+    accessToken,
+    user,
+    userDetails,
+    isLoading: isLoadingUser || isLoadingData,
+    subscription,
+  };
+  return <UserContext.Provider value={value} {...props}></UserContext.Provider>;
+};
+
+export const useUser = () => {
+  const context = useContext(UserContext);
+  if (context === null) {
+    throw new Error('useUser must be used within a UserContextProvider.');
+  }
+  return context;
+};
---
+++ providers/UserProvider.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import React from 'react';
+
+import { UserContextProvider } from '@/hooks/useUser';
+
+type UserProviderProps = {
+  children: React.ReactNode;
+};
+
+const UserProvider: React.FC<UserProviderProps> = ({ children }) => {
+  return <UserContextProvider>{children}</UserContextProvider>;
+};
+
+export default UserProvider;
