93d1974519731bb1c64ecb9a90c6433d393ab575
dineshyadav19 <dinesh.2108.yadav@gmail.com>
2023-10-04 20:37:19 +0530

    fix: remove side bar footer

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -52,7 +52,6 @@ const Sidebar: React.FC<SidebarProps> = ({ children, songs }) => {
         </Box>
         <Box className="h-full overflow-y-auto">
           <Library songs={songs} />
-          <SidebarFooter />
         </Box>
       </div>
       <main className="h-full overscroll-y-auto flex-1 py-2 px-2 md:pl-0">
