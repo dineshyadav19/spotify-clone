ea3bd8be718c68667e4983d0e09c4196377a17ef
dineshyadav19 <dinesh.2108.yadav@gmail.com>
2023-10-09 19:47:08 +0530

    fix: remove auth provider

--- components/Modal/AuthModal.tsx
+++ components/Modal/AuthModal.tsx
@@ -42,7 +42,7 @@ const AuthModal = () => {
         theme="dark"
         magicLink
         supabaseClient={supabaseClient}
-        //providers={['google']}
+        providers={[]}
         appearance={{
           theme: ThemeSupa,
           variables: {
